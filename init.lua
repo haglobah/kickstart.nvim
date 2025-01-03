947bdc75df424c20d3e9f103838e73820a2814e3
Beat Hagenlocher <bah@posteo.de>
2025-01-04 00:25:32 +0100

    Add neogit keybinding

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -927,7 +927,16 @@ require('lazy').setup({
       -- Only one of these is needed.
       'nvim-telescope/telescope.nvim', -- optional
     },
-    config = true
+    config = function ()
+      require("neogit").setup {
+        integrations = {
+          diffview = true,
+        },
+      }
+
+      -- Set keybinding
+      vim.keymap.set("n", "<leader>g", ":Neogit<CR>")
+    end,
   },
   {
     "Pocco81/auto-save.nvim",
